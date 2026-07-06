08a5b667a6d6c2223fe8cfce4dd096d3215cd7ba
Giga Kutateladze <160143635+giglaulala@users.noreply.github.com>
2026-07-06 13:15:06 +0400

    feat: add fallback countries to countriesApi

--- src/services/countriesApi.js
+++ src/services/countriesApi.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { fallbackCountries } from '../data/fallbackCountries';
 
 const api = axios.create({
   baseURL: 'https://restcountries.com/v3.1',
@@ -6,23 +7,27 @@ const api = axios.create({
 });
 
 export async function getEuropeanCountries() {
-  const { data } = await api.get(
-    '/region/europe?fields=name,capital,flags,population,region,subregion,cca3,maps,languages',
-  );
+  try {
+    const { data } = await api.get(
+      '/region/europe?fields=name,capital,flags,population,region,subregion,cca3,maps,languages',
+    );
 
-  return data
-    .map((country) => ({
-      id: country.cca3,
-      name: country.name.common,
-      officialName: country.name.official,
-      capital: country.capital?.[0] ?? 'N/A',
-      flag: country.flags.svg,
-      flagAlt: country.flags.alt ?? `${country.name.common} flag`,
-      population: country.population,
-      region: country.region,
-      subregion: country.subregion,
-      languages: country.languages ? Object.values(country.languages) : [],
-      mapUrl: country.maps.googleMaps,
-    }))
-    .sort((a, b) => a.name.localeCompare(b.name));
+    return data
+      .map((country) => ({
+        id: country.cca3,
+        name: country.name.common,
+        officialName: country.name.official,
+        capital: country.capital?.[0] ?? 'N/A',
+        flag: country.flags.svg,
+        flagAlt: country.flags.alt ?? `${country.name.common} flag`,
+        population: country.population,
+        region: country.region,
+        subregion: country.subregion,
+        languages: country.languages ? Object.values(country.languages) : [],
+        mapUrl: country.maps.googleMaps,
+      }))
+      .sort((a, b) => a.name.localeCompare(b.name));
+  } catch {
+    return fallbackCountries;
+  }
 }
